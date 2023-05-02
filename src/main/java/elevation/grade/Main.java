56c0ce5adfef0db2cbc5063a13601c8366547d83
Gyula Kiri <gyula.kiri@sap.com>
2023-05-02 14:15:43 +0200

    first element

--- src/main/java/elevation/grade/Main.java
+++ src/main/java/elevation/grade/Main.java
@@ -1,7 +1,20 @@
 package elevation.grade;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document doc = builder.parse(new File("src/main/resources/TdZ_hosszu_2022.gpx"));
+        doc.getDocumentElement().normalize();
+
+        System.out.println("Hello world!"+doc.getDocumentElement().getNodeName());
     }
 }
